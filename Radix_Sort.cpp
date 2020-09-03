20da5f8a72ac41b3fc6663b3fdc740e7c6b961f4
TheDangerD0802 <44977936+TheDangerD0802@users.noreply.github.com>
2020-09-03 20:06:09 +0530

    Radix_Sort with All pass

---
+++ Radix_Sort.cpp
@@ -0,0 +1,61 @@
+/*
+// Sample code to perform I/O:
+#include <stdio.h>
+
+int main(){
+	int num;
+	scanf("%d", &num);              			// Reading input from STDIN
+	printf("Input number is %d.\n", num);       // Writing output to STDOUT
+}
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+*/
+
+// Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+#define range 10
+void countsort(int arr[],int n,int place)
+{
+        int i,freq[range]={0};         //range for integers is 10 as digits range from 0-9
+        int output[n];
+        for(i=0;i<n;i++)
+                freq[(arr[i]/place)%range]++;
+        for(i=1;i<range;i++)
+                freq[i]+=freq[i-1];
+        for(i=n-1;i>=0;i--)
+        {
+                output[freq[(arr[i]/place)%range]-1]=arr[i];
+                freq[(arr[i]/place)%range]--;
+        }
+        for(i=0;i<n;i++)
+                arr[i]=output[i];
+
+		for(i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+
+		cout<<endl;
+}
+void radixsort(int arr[],int n,int maxx)            //maxx is the maximum element in the array
+{
+        int mul=1;
+        while(maxx)
+        {
+                countsort(arr,n,mul);
+                mul*=10;
+                maxx/=10;
+        }
+}
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+    int k = 0;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+        k = max(k,arr[i]);
+	}
+    radixsort(arr,n,k);
+}
